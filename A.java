39e721a688a40fb42bcea57532cc6607b4b85702
Akankshasingh8335 <152247756+Akankshasingh8335@users.noreply.github.com>
2023-12-02 22:06:22 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,17 @@
+package pack1;
+public class A
+{
+	private int x = 10;
+	protected int y=20;
+	int z=30;
+	public int s=40;
+	public A()
+	{
+		System.out.println(" members value in class A of pack 1 is: ");
+		System.out.println("private member: "+x);
+		System.out.println("protected member: "+y);
+		System.out.println("default member: "+z);
+		System.out.println("public member: "+s);
+	}	
+} 
+
