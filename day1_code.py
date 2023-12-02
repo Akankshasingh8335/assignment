7dbe03c425a60bf3f947aadedfe752ddd8b38f08
Akankshasingh8335 <akankshasingh8335@gmail.com>
2023-12-02 22:00:52 +0530

    adding new py file to git

---
+++ day1_code.py
@@ -0,0 +1,41 @@
+ x=10
+print(type(x))
+print(x)
+x="Hello"
+print(type(x))
+print(x)
+
+
+
+
+x=10
+print(type(x))
+print(x)
+x="Hello"
+print(type(x))
+print(x)
+
+
+
+
+
+#Write a program to add two numbers
+FN= int(input("Enter the First Number : "))
+SN= int(input("Enter the Second Number : "))
+result = FN+SN
+# Formatting String Output
+print("Result of Addition of",FN," and",SN," =",result)
+print("Result of Addition of "+str(FN)+" and "+str(SN)+" = "+str(result))
+print("Result of Addition %d and %d = %d " % (FN,SN,result))
+print("Result of Addition {0} and {1} = {2} ".format(FN,SN,result))
+
+#print(type(x))
+
+
+
+
+
+x="CDAC Noida\n\
+is in\n\
+sector 62"
+print(x)
